5ca4ad823d766129049305203cd05588c30f87a5
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-17 21:30:30 +0800

    Development mode

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -7,13 +7,15 @@ from app.utils.responses import error_response
 def guardian_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        try:
-            verify_jwt_in_request()
-            current_user_id = get_jwt_identity()
-            guardian = Guardian.query.get(current_user_id)
-            if not guardian:
-                return error_response("Guardian not found", 404)
-            return f(guardian, *args, **kwargs)
-        except Exception as e:
-            return error_response("Invalid token", 401)
+        # try:
+        #     # verify_jwt_in_request()
+        #     # current_user_id = get_jwt_identity()
+        #     guardian = Guardian.query.get(current_user_id)
+        #     if not guardian:
+        #         return error_response("Guardian not found", 404)
+        #     return f(guardian, *args, **kwargs)
+        # except Exception as e:
+        #     return error_response("Invalid token", 401)
+            dummy_guardian = None  # or Guardian.query.first()
+            return f(dummy_guardian, *args, **kwargs)
     return decorated_function
