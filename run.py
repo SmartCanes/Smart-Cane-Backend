4afe6d142322684ee15aac19e09bc256a851afb8
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-23 23:55:47 +0800

    Add a self signed certificate

--- run.py
+++ run.py
@@ -8,4 +8,9 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        host="0.0.0.0",         
+        port=5000,
+        ssl_context=("../server.cert", "../server.key") 
+    )
