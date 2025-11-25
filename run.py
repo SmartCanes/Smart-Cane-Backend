22d42747da4c6538bef407cc789d82aa285b8e7b
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-25 20:54:15 +0800

    Setup certificate environment

--- run.py
+++ run.py
@@ -8,9 +8,15 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=5000)
+    ssl_cert = os.getenv("PUBLIC_CERTIFICATE_KEY")
+    ssl_key = os.getenv("PRIVATE_CERTIFICATE_KEY")
+
+    ssl_context = None
+    if ssl_cert and ssl_key:
+        ssl_context = (ssl_cert, ssl_key)
+
     app.run(
-        host="0.0.0.0",         
+        host="0.0.0.0",
         port=5000,
-        ssl_context=("../server.cert", "../server.key") 
+        ssl_context=ssl_context
     )
