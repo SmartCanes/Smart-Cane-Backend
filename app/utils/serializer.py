0b265e2d4bf1e32dfd5104d54c36f63b76986b8d
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-12-21 21:21:50 +0800

    Modify serializer to include all by default

--- app/utils/serializer.py
+++ app/utils/serializer.py
@@ -3,21 +3,17 @@ def model_to_dict(model_instance, exclude_fields=None, include_fields=None):
         return None
 
     default_exclude = ["_sa_instance_state", "password"]
-
     if exclude_fields:
         default_exclude.extend(exclude_fields)
 
     data = {}
-    for key, value in model_instance.__dict__.items():
-        if key.startswith("_"):
-            continue
-
+    for column in model_instance.__table__.columns:
+        key = column.name
         if key in default_exclude:
             continue
-
         if include_fields and key not in include_fields:
             continue
-
+        value = getattr(model_instance, key)
         if hasattr(value, "isoformat"):
             data[key] = value.isoformat()
         else:
